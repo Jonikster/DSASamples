a0140a144289dfff6126dc60592f069e767ab9a1
Jonikster <john.berden4@gmail.com>
2026-01-12 19:28:33 +0000

    add source files

---
+++ Learning/LearningL/Program.cs
@@ -0,0 +1,12 @@
+﻿int[] nums = { 12, 5, 19, 63, 47, 28, 34, 16, 14 };
+/*
+Array.Sort(nums);
+Console.WriteLine($"Максимальный элемент: {nums[nums.Length-1]}");
+*/
+int max = 0;
+for (int i = 0; i < nums.Length; i++)
+{
+    if (nums[i] > max)
+        max = nums[i];
+}
+Console.WriteLine($"Максимальный элемент: {max}");
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # DSASamples
+These are samples for algorithms and data structures that I teach on my Youtube [here](https://www.youtube.com/watch?v=ABJ4jdTNEhU&list=PLSAG5rey4yDBzNl51iqiMHJ7JEBO7Yfqm).
